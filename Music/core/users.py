ec108490b96fca0285a55aafa45c2dab6da1dbf6
music-miko <voilet856@gmail.com>
2025-11-13 14:53:34 +0530

    Update users.py

--- Music/core/users.py
+++ Music/core/users.py
@@ -7,8 +7,8 @@ from .logger import LOGS
 class UsersData:
     def __init__(self) -> None:
         self.DEVS = [
-            1432756163,  # ForGo10God
-            1874070588,  # ForGo10_God
+            6848223695,  # ForGo10God
+            7321657753,  # ForGo10_God
         ]
 
     async def sudo_users(self):
